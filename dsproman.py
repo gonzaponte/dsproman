86749defe6c45199d868d2cf4da6b01054db87f1
Gonzalo <gonzaponte@gmail.com>
2020-03-11 22:19:28 +0200

    Add protection against bad value setting

--- dsproman.py
+++ dsproman.py
@@ -29,7 +29,12 @@ class SystemStateManager(SystemClient):
                     return
 
                 this.log(f"{time.asctime()} - Setting {device}.{attr} to {value}\n")
-                sup.__getattr__(device).__setattr__(attr, value)
+
+                changed = False
+                while not changed:
+                    sup.__getattr__(device).__setattr__(attr, value)
+                    changed = value == sup.__getattr__(device).__getattr__(attr)
+
                 if attr in "saved recording running clear_screen".split():
                     # These are not states properties, but rather operations
                     # TODO: change them from properties to methods
