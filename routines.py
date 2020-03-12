41eeaac8160f1ae2fc78308459291579b1a100fc
Gonzalo <gonzaponte@gmail.com>
2020-03-12 11:39:21 +0200

    Replace double function and use temporary in take_data

--- routines.py
+++ routines.py
@@ -1,17 +1,15 @@
+from . utils import temporary
 
-def data_taker(s):
-    def take_data(filename):
-        s.spectro       .save_path = f"{filename}_signal.asc"
-        s.power_meter   .save_path = f"{filename}_power.asc"
+def take_data(s, filename):
+    s.spectro    .save_path = f"{filename}_signal.asc"
+    s.power_meter.save_path = f"{filename}_power.asc"
 
-        s.power_meter.recording = True
-        s.spectro    .running   = True
-        s.power_meter.recording = False
+    with temporary(s, "power_meter", "recording", True):
+        s.spectro.running   = True
 
+    s.spectro    .saved     = True
+    s.power_meter.saved     = True
 
-        s.spectro       .saved     = True
-        s.power_meter   .saved     = True
-    return take_data
 
 
 def write_metadata(filename, crystal_mapping, rules):
