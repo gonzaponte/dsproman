4d53264c1221f55138ae47ce5f04fae5d20d83c1
Gonzalo <gonzaponte@gmail.com>
2020-03-12 11:39:50 +0200

    Add routines for ambient and background

--- routines.py
+++ routines.py
@@ -11,6 +11,28 @@ def take_data(s, filename):
     s.power_meter.saved     = True
 
 
+def take_ambient(s, filename):
+    with temporary(s, "source_shutter", "control", "computer"):
+        s.source_shutter.on         = False
+        s.spectro       .wavelength = 250 + 420 # 420 is approximately the middle of the wl range
+        s.spectro       .exposure   = 100
+
+        s.spectro.save_path = f"{filename}_signal.asc"
+        s.spectro.running   = True
+        s.spectro.saved     = True
+
+
+def take_background(s, filename):
+    with temporary(s, "source_shutter", "control", "computer"), \
+         temporary(s, "spectro", "shutter", "closed"):
+        s.source_shutter.on         = False
+        s.spectro       .wavelength = 250 + 420 # 420 is approximately the middle of the wl range
+        s.spectro       .exposure   = 100
+
+        s.spectro.save_path = f"{filename}_signal.asc"
+        s.spectro.running   = True
+        s.spectro.saved     = True
+
 
 def write_metadata(filename, crystal_mapping, rules):
     with open(filename, "w") as file:
