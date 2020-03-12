a7fe200071c2eada8588c290570ac5e51c825a5f
Gonzalo <gonzaponte@gmail.com>
2020-03-12 13:51:53 +0200

    Add database

---
+++ database.py
@@ -0,0 +1,18 @@
+from operator import itemgetter
+
+Sp_signals  = (250, 400),
+Qz_signals  = (250, 346),
+LiF_signals = (260, 331), (450, 680), (450, 530)
+BaF_signals = (250, 570), (320, 960)
+CaF_signals = (410, 580), (600, 770), (400, 770), (450, 630)
+
+signals = dict(
+Sp  =  Sp_signals,
+Qz  =  Qz_signals,
+LiF = LiF_signals,
+BaF = BaF_signals,
+CaF = CaF_signals,
+)
+
+excitations = {key: tuple(map(itemgetter[0], value)) for key, value in signals.items()}
+emmitions   = {key: tuple(map(itemgetter[1], value)) for key, value in signals.items()}
