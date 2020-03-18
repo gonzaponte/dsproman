bb126e7420f51875cfca179300ff7dd6b927d01c
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2020-03-18 14:12:59 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='dsproman',
+      version='0.0',
+      description='Some shit',
+      url='http://github.com/gonzaponte/dsproman',
+      author='Im ashamed',
+      author_email='',
+      license='MIT',
+      packages=['dsproman'],
+      install_requires=[
+      "devserve"
+      ],
+      zip_safe=False)
