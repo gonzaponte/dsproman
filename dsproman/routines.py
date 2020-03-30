64a136697fcc6380eaff361833f873bff6584530
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2020-03-30 16:04:12 +0300

    Add baseline data taking routine

--- dsproman/routines.py
+++ dsproman/routines.py
@@ -43,6 +43,18 @@ def take_background(s, filename):
         s.spectro.saved     = True
 
 
+def take_baseline(s, filename):
+    with temporary(s, "source_shutter", "control", "computer"), \
+         temporary(s, "spectro", "shutter", "closed"):
+        s.source_shutter.on         = False
+        s.spectro       .wavelength = 250 + 420 # 420 is approximately the middle of the wl range
+        s.spectro       .exposure   = 0.01
+
+        s.spectro.save_path = f"{filename}_signal.asc"
+        s.spectro.running   = True
+        s.spectro.saved     = True
+
+
 def write_metadata(filename, crystal_mapping, rules):
     with open(filename, "w") as file:
         text = """meta = {
