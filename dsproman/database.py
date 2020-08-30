02c52cf5d7603835040499a90c803f48f7ac7add
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2020-08-30 12:16:36 +0300

    Add empty entry for MgF to database

--- dsproman/database.py
+++ dsproman/database.py
@@ -7,6 +7,7 @@ Qz_signals  = (250, 346),
 LiF_signals = (260, 331), (450, 680), (450, 530)
 BaF_signals = (250, 570), (320, 960)
 CaF_signals = (410, 580), (600, 770), (400, 770), (450, 630)
+MgF_signals = ()
 
 signals = dict(
 Sp  =  Sp_signals,
@@ -14,6 +15,7 @@ Qz  =  Qz_signals,
 LiF = LiF_signals,
 BaF = BaF_signals,
 CaF = CaF_signals,
+MgF = MgF_signals,
 )
 
 excitations = {key: tuple(map(itemgetter(0), value)) for key, value in signals.items()}
